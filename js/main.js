6ecc6dbb561582e8cefee22fc5cc768d89514e5d
AhmedAlqiadi <148564666+AhmedAlqiadi@users.noreply.github.com>
2024-01-25 22:24:11 +0300

    commint js and html and css and images files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var humparger = document.querySelector('.humparger')
+var nav = document.querySelector('header nav .list-nav')
+
+humparger.addEventListener('click', ()=>{
+    nav.classList.toggle('show')
+})
